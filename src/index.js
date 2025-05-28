ab31bc35c59702f1b701e255d4b7e4daeb28f13e
Dmitriy Fedorenko <dmitriy.fedorenko13@gmail.com>
2025-05-28 21:30:19 +0300

    add webpack template & gh-pages

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import "./index.css";
+
+const App = () => {
+  return (
+    <section className="todoapp">
+      <header className="header">
+        <h1>todos</h1>
+        <input
+          className="new-todo"
+          placeholder="What needs to be done?"
+          autoFocus
+        />
+      </header>
+    </section>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
